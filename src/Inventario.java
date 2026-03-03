32b2eac878e430d6297775b85931688aa499ad79
Laura <lauraccastanedav@gmail.com>
2026-03-03 16:44:35 +0100

    getter y setter

--- src/Inventario.java
+++ src/Inventario.java
@@ -3,6 +3,19 @@ import java.util.ArrayList;
 public class Inventario {
 	private ArrayList <String> mascota;
 	
+	public Inventario(ArrayList<String> mascota) {
+		this.mascota = mascota;
+	}
+
+	
+	public ArrayList<String> getMascota() {
+		return mascota;
+	}
+
+	public void setMascota(ArrayList<String> mascota) {
+		this.mascota = mascota;
+	}
+	
 	
 	
 
